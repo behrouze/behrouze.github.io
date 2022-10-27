05b3db6ac41ce7274ca2920e20a50a3670bd0c1b
Beh <dev@behrouze.com>
2022-10-28 01:16:22 +0200

    Updates

---
+++ mtm.js
@@ -0,0 +1,4 @@
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({ 'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start' });
+var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+g.async = true; g.src = 'https://analytics.behrouze.com:443/js/container_Rogew4vI.js'; s.parentNode.insertBefore(g, s);
